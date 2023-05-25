293c41d25d4818750df4f5ff22caccfa7381b3f3
Alex <aekazak@yandex.ru>
2023-05-25 19:03:18 +0300

    Webpack image compression changed

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
     new ImageMinimizerPlugin({
       minimizer: {
         implementation: ImageMinimizerPlugin.imageminMinify,
-        options: { plugins: [['imagemin-mozjpeg', { quality: 50 }]] },
+        options: { plugins: [['imagemin-mozjpeg', { quality: 65 }]] },
       },
     }),
   ];
